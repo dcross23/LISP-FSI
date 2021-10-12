3a8b06bc83c1e93a4cef030edddf4b258d628789
David Cruz García <david.cruz.garciaa@gmail.com>
2021-10-12 13:00:07 +0200

    Updated tests

--- src/tests.lisp
+++ src/tests.lisp
@@ -10,15 +10,18 @@
 
 (setf passed_tests 0)
 (setf failed_tests 0)
+(setf total_tests 0)
 
 (defun test_es_atomo(elto result)
     (if (equal (es_atomo elto) result)
         (progn
             (setf passed_tests (1+ passed_tests))
+            (setf total_tests (1+ total_tests))
             'PASSED
         )
         (progn
             (setf failed_tests (1+ failed_tests))
+            (setf total_tests (1+ total_tests))
             'FAILED
         )
     )
@@ -28,10 +31,12 @@
     (if (equal (es_variable elto) result)
         (progn
             (setf passed_tests (1+ passed_tests))
+            (setf total_tests (1+ total_tests))
             'PASSED
         )
         (progn
             (setf failed_tests (1+ failed_tests))
+            (setf total_tests (1+ total_tests))
             'FAILED
         )
     )
@@ -41,10 +46,12 @@
     (if (equal (es_lista elto) result)
         (progn
             (setf passed_tests (1+ passed_tests))
+            (setf total_tests (1+ total_tests))
             'PASSED
         )
         (progn
             (setf failed_tests (1+ failed_tests))
+            (setf total_tests (1+ total_tests))
             'FAILED
         )
     )
@@ -54,10 +61,12 @@
     (if (equal (contiene lista elto) result)
         (progn
             (setf passed_tests (1+ passed_tests))
+            (setf total_tests (1+ total_tests))
             'PASSED
         )
         (progn
             (setf failed_tests (1+ failed_tests))
+            (setf total_tests (1+ total_tests))
             'FAILED
         )
     )
@@ -67,10 +76,12 @@
     (if (equal (get_sustitucion elto lista_sust) result)
         (progn
             (setf passed_tests (1+ passed_tests))
+            (setf total_tests (1+ total_tests))
             'PASSED
         )
         (progn
             (setf failed_tests (1+ failed_tests))
+            (setf total_tests (1+ total_tests))
             'FAILED
         )
     )
@@ -80,10 +91,12 @@
     (if (equal (aplicacion lista_sust elto) result)
         (progn
             (setf passed_tests (1+ passed_tests))
+            (setf total_tests (1+ total_tests))
             'PASSED
         )
         (progn
             (setf failed_tests (1+ failed_tests))
+            (setf total_tests (1+ total_tests))
             'FAILED
         )
     )
@@ -93,10 +106,12 @@
     (if (equal (composicion S1 S2) result)
         (progn
             (setf passed_tests (1+ passed_tests))
+            (setf total_tests (1+ total_tests))
             'PASSED
         )
         (progn
             (setf failed_tests (1+ failed_tests))
+            (setf total_tests (1+ total_tests))
             'FAILED
         )
     )
@@ -106,24 +121,26 @@
     (if (equal (unificacion E1 E2) result)
         (progn
             (setf passed_tests (1+ passed_tests))
+            (setf total_tests (1+ total_tests))
             'PASSED
         )
         (progn
             (setf failed_tests (1+ failed_tests))
+            (setf total_tests (1+ total_tests))
             'FAILED
         )
     )
 )
 
 (defun imprimir_unificacion(E1 E2)
-    (princ "  E1: ") (write E1) (terpri)
-    (princ "  E2: ") (write E2) (terpri)
+    (princ "    E1: ") (write E1) (terpri)
+    (princ "    E2: ") (write E2) (terpri)
     (setf res (unificacion E1 E2))
-    (princ "  Resultado: ") (write res) (terpri)
+    (princ "    Resultado: ") (write res) (terpri)
 
     (when (not(equal res 'FALLO))
-        (princ "  E1 tras unificar: ") (write (aplicacion res E1)) (terpri)
-        (princ "  E2 tras unificar: ") (write (aplicacion res E2)) (terpri)
+        (princ "    E1 tras unificar: ") (write (aplicacion res E1)) (terpri)
+        (princ "    E2 tras unificar: ") (write (aplicacion res E2)) (terpri)
     )
 )
 
@@ -137,6 +154,7 @@
 (princ "  ") (write (test_es_atomo '(A (? b)) NIL)) (terpri)
 (write-line "")
 
+
 (write-line "------------------")
 (write-line "Tests es_variable:")
 (write-line "------------------")
@@ -146,6 +164,7 @@
 (princ "  ") (write (test_es_variable '(1 2) NIL))  (terpri)
 (princ "  ") (write (test_es_variable '(A (? b)) NIL))  (terpri)
 (write-line "")
+
 
 (write-line "------------------")
 (write-line "Tests es_lista:")
@@ -157,19 +176,26 @@
 (princ "  ") (write (test_es_lista '(A (? b)) T))  (terpri)
 (write-line "")
 
+
 (write-line "------------------")
 (write-line "Tests contiene:")
 (write-line "------------------")
 (princ "  ") (write (test_contiene '(1 2 3) '1 T))  (terpri)
 (princ "  ") (write (test_contiene '(A (? b)) '(? c) NIL))  (terpri)
+(princ "  ") (write (test_contiene '(A (? b)) '(? b) T))  (terpri)
+(princ "  ") (write (test_contiene '((1 2) (A (? x)) 3) '(? x) T))  (terpri)
+(princ "  ") (write (test_contiene '((1 5) (4 (1 (9 2))) 3) '2 T))  (terpri)
 (write-line "")
+
 
 (write-line "------------------")
 (write-line "Tests get_sustitucion:")
 (write-line "------------------")
 (princ "  ") (write (test_get_sustitucion '(? x) '((A (? x)) (B (? y))) 'A)) (terpri)
 (princ "  ") (write (test_get_sustitucion '(f (? x) (? y)) '((A (? x)) (W (f (? x) (? y)))) 'W)) (terpri)
+(princ "  ") (write (test_get_sustitucion '((? x) (? y)) '((A (? x)) (W (? y))) '((? x) (? y)))) (terpri)
 (write-line "")
+
 
 (write-line "------------------")
 (write-line "Tests aplicacion:")
@@ -183,7 +209,13 @@
 (setf elto '((f (? x)) (? x) (g (? y)) (? w)) )
 (setf result '((f A) A (g B) C))
 (princ "  ") (write (test_aplicacion lsust elto result )) (terpri)
+
+(setf lsust '((A (? x)) (B (? y)) (C (? w)) (D (? z))) )
+(setf elto '((f (g (? y) (? x))) (? x) (g (? w)) (? r)) )
+(setf result '((f (g B A)) A (g C) (? r)) )
+(princ "  ") (write (test_aplicacion lsust elto result )) (terpri)
 (write-line "")
+
 
 (write-line "------------------")
 (write-line "Tests composicion:")
@@ -197,7 +229,13 @@
 (setf S2 '((A (? x)) (B (? y)) (C (? w)) (D (? z))) )
 (setf result '( ((f A) (? x)) ((g B) (? w)) (B (? y)) (D (? z)) ) )
 (princ "  ") (write (test_composicion S1 S2 result )) (terpri)
+
+(setf S1 '(((f (g (? y) (? x))) (? w))  ((g (? z)) (? z))) )
+(setf S2 '((A (? x)) (B (? y)) (C (? w)) (D (? z))) )
+(setf result '( ((f (g B A)) (? w)) ((g D) (? z)) (A (? x)) (B (? y))) )
+(princ "  ") (write (test_composicion S1 S2 result )) (terpri)
 (write-line "")
+
 
 (write-line "------------------")
 (write-line "Tests unificacion:")
@@ -214,23 +252,35 @@
 (imprimir_unificacion E1 E2)
 (princ " ") (write (test_unificacion E1 E2 result )) (terpri) (terpri)
 
-(setf E1 '(P (? X) (F (? X))) )
+(setf E1 '(P (? x) (F (? x))) ) 
 (setf E2 '(P A C D) )
 (setf result 'FALLO )
 (imprimir_unificacion E1 E2)
 (princ " ") (write (test_unificacion E1 E2 result )) (terpri) (terpri)
 
-(setf E1 '(P (? X) (F A)) )
+(setf E1 '(P (? x) (F A)) )
 (setf E2 '(P (g B) (? y)) )
 (setf result '( ((g B) (? x)) ((f A) (? y)) ) )
 (imprimir_unificacion E1 E2)
 (princ " ") (write (test_unificacion E1 E2 result )) (terpri) (terpri)
 
+(setf E1 '(P (? x) (? w) (F (? w)) (F A)) )
+(setf E2 '(P (g B) C (F C) (? y)) )
+(setf result '( ((g B) (? x)) (C (? w)) ((f A) (? y)) ) )
+(imprimir_unificacion E1 E2)
+(princ " ") (write (test_unificacion E1 E2 result )) (terpri) (terpri)
 
-(terpri)
+(setf E1 '(P (? x) (? w) (F (? w)) (F A)) )
+(setf E2 '(P (g B) C (F C A) (? y)) )
+(setf result 'FALLO )
+(imprimir_unificacion E1 E2)
+(princ " ") (write (test_unificacion E1 E2 result )) (terpri) (terpri)
+(write-line "")
+
 (write-line "------------------")
-(write-line "RESULTS")
+(write-line "RESULTADOS")
 (write-line "------------------")
+(princ "TOTAL:        ") (write total_tests) (terpri)
 (princ "Tests passed: ") (write passed_tests) (terpri)
 (princ "Tests failed: ") (write failed_tests) (terpri)
 
